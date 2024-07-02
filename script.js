9e1b910822751340fce44940212c391d5496e689
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-02 19:08:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+document.addEventListener('DOMContentLoaded', (event) => {
 const codes = Array.from(document.querySelectorAll('.code'));
-codes[0].focus();
-codes.forEach((code,index)=>{
-	 code.addEventListener('input', () => {
-        if (code.value.length>0) {
-          if (index<codes.length-1) {
-            codes[index+1].focus();
-          }
-        }
-      });
-	
-    code.addEventListener("keydown",(e)=>{
-        const keyPressed = e.key
-         if(keyPressed==="Backspace"){
-            if(index>0){
-                codes[index-1].focus();
-            }
-        }
-    })
-})
+	codes.forEach((code,index)=>{
+		 code.addEventListener('input', () => {
+	        if (code.value.length>0) {
+	          if (index<codes.length-1) {
+	            codes[index+1].focus();
+	          }
+	        }
+	      });
+		
+	    code.addEventListener("keydown",(e)=>{
+	        const keyPressed = e.key
+	         if(keyPressed==="Backspace"){
+	            if(index>0){
+	                codes[index-1].focus();
+	            }
+	        }
+	    })
+	})
+});
