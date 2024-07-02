b316bdec4b90f4fcec7b46dc6171c5e6b11a92ae
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-02 20:52:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 const codes = Array.from(document.querySelectorAll('.code'));
-	codes.forEach((code,index)=>{
-		 
-		code.addEventListener("input", () => {
-	        if (code.value.length>0) {
-	          if (index<codes.length-1) {
-	            codes[index+1].focus();
-	          }
-	        }
-	      });
-		
+	codes.forEach((code,index)=>{				
 	    code.addEventListener("keydown",(e)=>{
 	        const keyPressed = e.key
+			if(keyPressed>=0 && keyPressed<=9){
+            codes[index].value=''
+            if(index<codes.length-1){
+                setTimeout(()=>codes[index+1].focus(),10)
+            }
+	        }
 	         if(keyPressed==="Backspace"){
 	            if(index>0){
 	                codes[index-1].focus();
